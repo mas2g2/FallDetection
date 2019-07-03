d51baf8f0649ed0f2f989c4731a002256390538a
mas2g2 <mas2g2@mail.missouri.edu>
2019-07-02 23:42:21 -0500

    updated file

--- ml_project.py
+++ ml_project.py
@@ -26,9 +26,33 @@ def split_data(data,train=0.5,test=0.5):
     test = data[train_len,:]
     # Returns testing data and training data
     return train,test
-    
+
+def mean(data):
+    sum_arr = np.zeros((1,6))
+    for i in range(16382):
+        sum_arr += data[i,1:]
+    mean = sum_arr/16382
+    return mean
+
+def cov(data,mean):
+    diff = np.zeros((6,6))
+    for i in range(len(data)):
+        diff += (data[i,1:] - mean).T.dot(data[i,1:] - mean)
+    cov = diff/16382
+    return cov
+
 data = load_data("falldetection.csv");
 shape = data.shape
-print(shape[0])
-train,test = split_data(data,train=0.8,test=0.2)
-print(train)
+mean = mean(data)
+cov = cov(data,mean)
+print("Mean: ",mean)
+print("Covariance: ",cov)
+w,v = np.linalg.eig(cov)
+print("Weights :\n",w);
+print("Vectors : \n",v)
+print("Proportion of variance with first three eig val : ",np.sum(w[:3])/np.sum(w))
+print(w[:3])
+W = v[:,:3]
+print(W)
+#train,test = split_data(data,train=0.8,test=0.2)
+
